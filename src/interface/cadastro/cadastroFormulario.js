237a0aa68eadd4be94d0c24169ff94b9d8931f1f
obsonferreira <obsonpereira@gmail.com>
2026-08-12 09:52:40 -0300

    refactor: separando responsabilidade modalContatoSalvo

--- src/interface/cadastro/cadastroFormulario.js
+++ src/interface/cadastro/cadastroFormulario.js
@@ -1,9 +1,5 @@
 import { salvarContato } from "../../servicos/agendaService.js";
-import { modalContatoSalvo } from "../compartilhado/dialog.js";
 
 export function enviarFormulario(dados) {
-    const resultado = salvarContato(dados);
-    console.log(resultado);
-    
-    modalContatoSalvo(resultado.mensagem);
+    return salvarContato(dados);
 };
